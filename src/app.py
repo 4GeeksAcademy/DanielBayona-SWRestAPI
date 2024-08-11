ca0d2cf17bca6e7042a2cb5899b02408af5efb8c
Daniel Bayona <65182005+Bayunas@users.noreply.github.com>
2024-08-11 17:50:36 +0000

    Finished SW API without authentication

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, People, Planet, User, Favorite
+import requests
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +45,202 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# end point to populate the table people with info of the API swapi.tech
+@app.route('/people/populatedb', methods=["GET"])
+def populatePeopledb():
+    response = requests.get('https://www.swapi.tech/api/people?page=1&limit=300')
+    response = response.json()
+    response = response.get("results")
+    
+    for item in response:
+        result = requests.get(item.get('url'))
+        result = result.json()
+        result = result.get('result')
+        people = People()
+        people.name = result.get('properties').get('name')
+        people.birth_year = result.get('properties').get('birth_year')
+        people.eye_color = result.get('properties').get('eye_color')
+        people.gender = result.get('properties').get('gender')
+        people.hair_color = result.get('properties').get('hair_color')
+        people.height  = result.get('properties').get('height')
+        people.mass  = result.get('properties').get('mass')
+        people.skin_color = result.get('properties').get('skin_color')
+        db.session.add(people)
+    try:
+        db.session.commit()
+        return jsonify('personajes agregados'), 200
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify('Error'), 500
+
+# end point to populate the table planet with info of the API swapi.tech    
+@app.route('/planet/populatedb', methods=["GET"])
+def populatePlanetdb():
+    response = requests.get('https://www.swapi.tech/api/planets?page=1&limit=300')
+    response = response.json()
+    response = response.get('results')
+    
+    for item in response:
+        result = requests.get(item.get('url'))
+        result = result.json()
+        result = result.get('result')
+        planet = Planet()
+        planet.name = result.get('properties').get('name')
+        planet.climate = result.get('properties').get('climate')
+        planet.diameter = result.get('properties').get('diameter')
+        planet.gravity = result.get('properties').get('gravity')
+        planet.orbital_period = result.get('properties').get('orbital_period')
+        planet.rotation_period = result.get('properties').get('orbital_period')
+        planet.surface_water = result.get('properties').get('surface_water')
+        planet.population = result.get('properties').get("population")
+        planet.terrain = result.get('properties').get('terrain')
+        db.session.add(planet)
+    try:
+        db.session.commit()
+        return jsonify('Planetas agregados'), 200
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify('Error'), 500
+    
+
+#Get all users with a response of a list of dictionaries 
+@app.route('/users', methods=['GET'])
+def getAllUsers():
+    users = User()
+    users = users.query.all()
+    users = list(map(lambda item: item.serialize(), users))
+    
+    return jsonify(users),200
+
+#Get all favorites of the actual user
+@app.route('/users/favorites/<int:favorite_id>', methods=['GET'])
+def getAllFavorites(favorite_id):
+    user = User()
+    user = user.query.filter_by(id=favorite_id).first()
+    
+    return jsonify(user.serialize_favorite()), 200
+
+# adds a new planet to the favorite users table
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def addPlanetFavorite(planet_id):
+    userId = 1
+    favorite = Favorite()
+    favorite.user_id = userId
+    favorite.planet_id = planet_id
+    
+    db.session.add(favorite)
+    try:
+        db.session.commit()
+        return jsonify('Planeta agreado a favoritos'), 200
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return ('error al agregar planeta a favoritos'), 201
+# adds a new person to the favorite users table
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def addPersonFavorite(people_id):
+    userId = 1
+    favorite = Favorite()
+    favorite.user_id = userId
+    favorite.people_id = people_id
+    
+    db.session.add(favorite)
+    
+    try:
+        db.session.commit()
+        return jsonify('Persona agregada a favorito'), 200
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify('Error al agregar personaje a favoritos'), 201
+    
+#Deletes the planet favroite by the ID
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def deletePlanetFavorite(planet_id):
+    userId = 1
+    favorite = Favorite()
+    
+    favorite = favorite.query.filter_by(user_id=userId, planet_id=planet_id).first()
+    
+    if favorite:
+        db.session.delete(favorite)
+    
+        try:
+            db.session.commit()
+            return jsonify('planeta eliminado de favoritos'), 200
+        except Exception as error:
+            print(error)
+            db.session.rollback()
+            return jsonify('No se pudo eliminar planeta de favorito'), 201
+    else:
+        return jsonify('Planeta favorito no encontrado'), 404
+
+#Deletes the person favroite by the ID
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def deletePersonFavorite(people_id):
+    userId = 1
+    favorite = Favorite()
+    favorite = favorite.query.filter_by(user_id = userId, people_id = people_id).first()
+    
+    if favorite:
+        db.session.delete(favorite)
+        
+        try:
+            db.session.commit()
+            return jsonify('Personaje eliminado de favoritos'), 200
+        except Exception as error:
+            print(error)
+            return jsonify('Error al eliminar personaje de favoritos'), 201
+    else:
+        return jsonify('No se ecnontro el personaje en favoritos'), 404
+    
+#Get all people with a response of a list of dictionaries 
+@app.route('/people', methods=['GET'])
+def getAllPeople():
+    people = People()
+    people = people.query.all()
+    people = list(map(lambda item: item.serialize(), people))
+    
+    return jsonify(people), 200
+
+#Get a specific person with a repsonse of all the info with the ID
+@app.route('/people/<int:people_id>', methods=['GET']) 
+def getPerson(people_id):
+    person = People()
+    person = person.query.get(people_id)
+    
+    if person is None:
+        raise APIException('Person not found', status_code=404)
+    else:
+        return jsonify(person.serialize()),200
+    
+#Get all planets with a response of a list of dictionaries
+@app.route('/planets', methods=['GET'])
+def getAllPlantes():
+    planet = Planet()
+    planet = planet.query.all()
+    planet = list(map(lambda item: item.serialize(), planet))
+    
+    return jsonify(planet), 200
+
+
+#Get a specific planet with a repsonse of all the info with the ID
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def getPlanet(planet_id):
+    planet = Planet()
+    planet = planet.query.get(planet_id)
+
+    if planet is None:
+        raise APIException('Planet not found', status_code=404)
+    else:
+        return jsonify(planet.serialize()), 200
+    
+
+
+        
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,12 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime, timezone
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable = False)
+    last_name = db.Column(db.String(20), nullable = False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable = False, default = datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, nullable = False, default = datetime.now(timezone.utc), onupdate = datetime.now(timezone.utc))
+    
+    favorites = db.relationship('Favorite', back_populates = 'user')
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +21,89 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name
             # do not serialize the password, its a security breach
+        }
+    def serialize_favorite(self):
+        return{
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "favorites": list(map(lambda item: item.serialize(), self.favorites))
+        }
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable = False)
+    height = db.Column(db.String(20), nullable = False)
+    mass = db.Column(db.String(20), nullable = False)
+    hair_color = db.Column(db.String(20), nullable = False)
+    skin_color = db.Column(db.String(20), nullable = False)
+    eye_color = db.Column(db.String(50), nullable = False)
+    birth_year = db.Column(db.String(50), nullable = False)
+    gender = db.Column(db.String(50), nullable = False)
+    created = db.Column(db.DateTime, nullable = False, default = datetime.now(timezone.utc))
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "created": self.created
+        }
+        
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    climate = db.Column(db.String(50), nullable = False)
+    diameter = db.Column(db.String(50), nullable = False)
+    gravity = db.Column(db.String(50), nullable=False)
+    orbital_period = db.Column(db.String(50), nullable=False)
+    rotation_period = db.Column(db.String(50), nullable = False)
+    population = db.Column(db.String(50), nullable = False)
+    terrain = db.Column(db.String(50), nullable = False)
+    surface_water = db.Column(db.String(50), nullable = False)
+    created = db.Column(db.DateTime, nullable = False, default= datetime.now(timezone.utc))
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "population": self.population,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "created": self.created
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key = True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable = True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    
+    user = db.relationship("User", back_populates = 'favorites')
+    planet = db.relationship('Planet')
+    people = db.relationship('People')
+    
+    def serialize(self):
+        return{
+            "people_id": self.people_id,
+            "planet_id": self.planet_id,
+            "user_id": self.user_id,
+            "planet_name": self.planet.name if self.planet else None,
+            "people_name": self.people.name if self.people else None
         }
